294c5bc37d3446dfdff2c649b475f6252ac8da88
michael-n-cooper <cooper@w3.org>
2017-06-30 13:55:17 -0400

    restage

--- guidelines/respec-config.js
+++ guidelines/respec-config.js
@@ -11,7 +11,7 @@ var respecConfig = {
 	specStatus:           "WD",
 	//crEnd:                "2012-04-30",
 	//perEnd:               "2013-07-23",
-	publishDate:          "2017-06-29",
+	publishDate:          "2017-06-30",
 	diffTool:             "http://www.aptest.com/standards/htmldiff/htmldiff.pl",
 	
 	// the specifications short name, as in https://www.w3.org/TR/short-name/
