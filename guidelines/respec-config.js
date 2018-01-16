122447b51fbaa325d6d4a52cc45f58a4795a79f7
michael-n-cooper <cooper@w3.org>
2018-01-16 11:23:52 -0500

    Config for CR

--- guidelines/respec-config.js
+++ guidelines/respec-config.js
@@ -8,10 +8,11 @@ var respecConfig = {
 	permalinkHide:     false,
 	tocIntroductory: true,
 	// specification status (e.g., WD, LC, NOTE, etc.). If in doubt use ED.
-	specStatus:           "ED",
-	//crEnd:                "2012-04-30",
+	specStatus:           "CR",
+	crEnd:                "2018-03-30",
+	implementationReportURI: "https://www.w3.org/WAI/WCAG21/implementation-report/",
 	//perEnd:               "2013-07-23",
-	//publishDate:          "2013-08-22",
+	publishDate:          "2018-01-25",
 	diffTool:             "http://www.aptest.com/standards/htmldiff/htmldiff.pl",
 	
 	// the specifications short name, as in https://www.w3.org/TR/short-name/
@@ -25,9 +26,9 @@ var respecConfig = {
 	
 	// if there is a previously published draft, uncomment this and set its YYYY-MM-DD date
 	// and its maturity status
-	//previousPublishDate:  "2014-06-12",
-	//previousMaturity:  "WD",
-	prevRecURI: "https://www.w3.org/TR/2008/REC-WCAG20-20081211/",
+	previousPublishDate:  "2018-12-07",
+	previousMaturity:  "WD",
+	prevRecURI: "https://www.w3.org/TR/WCAG20/",
 	//previousDiffURI: "https://www.w3.org/TR/2014/REC-wai-aria-20140320/",
 	
 	// if there a publicly available Editors Draft, this is the link
