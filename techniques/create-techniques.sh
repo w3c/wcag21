34103517ba49b36b3524560fc8d4b60b995e8911
michael-n-cooper <cooper@w3.org>
2018-05-02 15:36:45 -0400

    filling out the script

--- techniques/create-techniques.sh
+++ techniques/create-techniques.sh
@@ -3,9 +3,16 @@ echo cp technique-template.html ../../technique-template.html
 echo cd ..
 while [ $# -gt 0 ]
 do
-  echo git checkout --orphan $2
+  echo git checkout --orphan tech-$2
   echo git rm -rf .
-  echo cp ../technique-template.html $1/$2.html
+  echo cp ../technique-template.html techniques/$1/$2.html
+  echo sed -i "s/Technique Title/$3/g" techniques/$1/$2.html
+  echo git add .
+  echo git commit -m "Set up $2 technique"
+  echo git push --set-upstream origin tech-$2
+  shift
   shift
   shift
 done
+echo rm ../technique-template.html
+echo git checkout technique-setup
