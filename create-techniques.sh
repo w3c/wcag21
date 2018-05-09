1cbd70a4b4623d1dfbc8d10c8bc3e5c530d136dc
michael-n-cooper <cooper@w3.org>
2018-05-08 22:16:49 -0400

    more

--- create-techniques.sh
+++ create-techniques.sh
@@ -8,11 +8,12 @@ while [ $# -gt 0 ]
 do
   git checkout --orphan tech-$2
   git rm -rf .
+  mkdir techniques
   mkdir techniques/$1
   cp ../technique-template.html techniques/$1/$2.html
-  sed -i "s/<p id=\"technology\"></p>/<p id=\"technology\">$1<\/p>/" techniques/$1/$2.html
-  sed -i "s/<p id=\"type\"></p>/<p id=\"type\">$3<\/p>/" techniques/$1/$2.html
-  sed -i "s/Technique Title/$4/g" techniques/$1/$2.html
+  sed -i "s|<p id=\"technology\"></p>|<p id=\"technology\">$1</p>|" techniques/$1/$2.html
+  sed -i "s|<p id=\"type\"></p>|<p id=\"type\">$3</p>|" techniques/$1/$2.html
+  sed -i "s|Technique Title|$4|g" techniques/$1/$2.html
   git add techniques/$1/$2.html
   git commit -m "Set up $2 technique"
 #  git push --set-upstream origin tech-$2
