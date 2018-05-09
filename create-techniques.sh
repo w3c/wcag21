66a3340c2b0f2d60d17f8c407047cc1653fc11b6
michael-n-cooper <cooper@w3.org>
2018-05-08 20:51:18 -0400

    add metadata support

--- create-techniques.sh
+++ create-techniques.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# $1 = technology
+# $2 = filename
+# $3 = type
+# $4 = title
 cp techniques/technique-template.html ../technique-template.html
 while [ $# -gt 0 ]
 do
@@ -6,10 +10,13 @@ do
   git rm -rf .
   mkdir techniques/$1
   cp ../technique-template.html techniques/$1/$2.html
-  sed -i "s/Technique Title/$3/g" techniques/$1/$2.html
+  sed -i "s/<p id=\"technology\"></p>/<p id=\"technology\">$1</p>/" techniques/$1/$2.html
+  sed -i "s/<p id=\"type\"></p>/<p id=\"type\">$3</p>/" techniques/$1/$2.html
+  sed -i "s/Technique Title/$4/g" techniques/$1/$2.html
   git add techniques/$1/$2.html
   git commit -m "Set up $2 technique"
   git push --set-upstream origin tech-$2
+  shift
   shift
   shift
   shift
