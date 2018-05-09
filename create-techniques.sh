c964ef7f1c707b770b0ee6c9a65998d140c89c43
michael-n-cooper <cooper@w3.org>
2018-05-08 22:14:51 -0400

    some corrections

--- create-techniques.sh
+++ create-techniques.sh
@@ -10,12 +10,12 @@ do
   git rm -rf .
   mkdir techniques/$1
   cp ../technique-template.html techniques/$1/$2.html
-  sed -i "s/<p id=\"technology\"></p>/<p id=\"technology\">$1</p>/" techniques/$1/$2.html
-  sed -i "s/<p id=\"type\"></p>/<p id=\"type\">$3</p>/" techniques/$1/$2.html
+  sed -i "s/<p id=\"technology\"></p>/<p id=\"technology\">$1<\/p>/" techniques/$1/$2.html
+  sed -i "s/<p id=\"type\"></p>/<p id=\"type\">$3<\/p>/" techniques/$1/$2.html
   sed -i "s/Technique Title/$4/g" techniques/$1/$2.html
   git add techniques/$1/$2.html
   git commit -m "Set up $2 technique"
-  git push --set-upstream origin tech-$2
+#  git push --set-upstream origin tech-$2
   shift
   shift
   shift
